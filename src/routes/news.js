4f33900398009fc9d49812e92726fcbdea7fe68b
quynhhtm <manhquynh458@gmail.com>
2024-06-24 00:28:42 +0700

    Install prettier, lint-staged, husky

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const route = express.Router();
 
-const newsController = require('../app/controllers/NewsController')
+const newsController = require('../app/controllers/NewsController');
 // newsController.index
 
-route.use('/:slug', newsController.show)
-route.use('/', newsController.index)
+route.use('/:slug', newsController.show);
+route.use('/', newsController.index);
 
 module.exports = route;
